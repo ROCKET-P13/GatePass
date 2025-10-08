8be98dcb6ba2495f6a1c20de271bb47c5b03af33
ROCKET-P13 <135278016+ROCKET-P13@users.noreply.github.com>
2025-10-08 06:45:41 -0400

    feat: aws cdk boilerplate

---
+++ MotoPassInfrastructure/test/moto_pass_infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MotoPassInfrastructure from '../lib/moto_pass_infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/moto_pass_infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MotoPassInfrastructure.MotoPassInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

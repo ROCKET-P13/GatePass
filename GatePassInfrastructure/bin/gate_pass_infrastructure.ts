ed8a38073838fec680828c34dcf749e6a90de22e
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-01-06 06:24:33 -0500

    update: MotoPass => GatePass

--- GatePassInfrastructure/bin/gate_pass_infrastructure.ts
+++ GatePassInfrastructure/bin/gate_pass_infrastructure.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { MotoPassInfrastructureStack } from '../lib/moto_pass_infrastructure-stack';
+import { GatePassInfrastructureStack } from '../lib/gate_pass_infrastructure-stack';
 
 const app = new cdk.App();
-new MotoPassInfrastructureStack(app, 'MotoPassInfrastructureStack', {
+new GatePassInfrastructureStack(app, 'GatePassInfrastructureStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- GatePassInfrastructure/test/moto_pass_infrastructure.test.ts
+++ GatePassInfrastructure/test/moto_pass_infrastructure.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as MotoPassInfrastructure from '../lib/moto_pass_infrastructure-stack';
+// import * as GatePassInfrastructure from '../lib/gate_pass_infrastructure-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/moto_pass_infrastructure-stack.ts
+// example resource in lib/gate_pass_infrastructure-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new MotoPassInfrastructure.MotoPassInfrastructureStack(app, 'MyTestStack');
+//   const stack = new GatePassInfrastructure.GatePassInfrastructureStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
